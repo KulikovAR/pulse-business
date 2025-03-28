4c597918eb8298806d8609781602e8c606b88a47
ladick1337 <76527456+ladick1337@users.noreply.github.com>
2025-03-28 14:24:33 +0300

    tg back-btn

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -7,5 +7,18 @@
 
 export default {
     name: 'App',
+    mounted() {
+        const telegram = window.Telegram.WebApp;
+        telegram.ready();
+
+        // Включаем кнопку "Назад"
+        telegram.BackButton.show();
+
+        // Устанавливаем обработчик для нажатия на кнопку "Назад"
+        telegram.BackButton.onClick(() => {
+        // Ваша логика при нажатии кнопки "Назад"
+            this.$router.go(-1);
+        });
+    }
 };
 </script>
